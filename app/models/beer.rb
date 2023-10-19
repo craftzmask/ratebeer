42f49b436c5d4401dba6ddf3e5afc3e970aa98a9
Khanh Chung <khanh.d.chung@gmail.com>
2023-10-18 17:03:02 -0700

    week2 ex3 and ex4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,13 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    total_scores = 0.0
+    ratings.each do |rating|
+      total_scores += rating.score
+    end
+
+    (total_scores / ratings.count)
+  end
 end
