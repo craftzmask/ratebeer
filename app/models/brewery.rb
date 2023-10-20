5a9b62871ab4544271c9b7a26abeb867e521c6fc
Khanh Chung <khanh.d.chung@gmail.com>
2023-10-19 20:21:13 -0700

    week2 ex14

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,8 @@
 class Brewery < ApplicationRecord
   has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
+
+  def average_rating
+    ratings.count > 0 ? ratings.average(:score) : 0.0
+  end
 end
